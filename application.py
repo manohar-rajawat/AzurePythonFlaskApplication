8c0fcc7f905ed11df908ea633754d4c8880500c2
manohar singh rajawat <manoharsingh1920@gmail.com>
2020-04-19 01:02:37 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    message = "! Jai Mahakal !"
-    return render_template('homepage.html')
+    return "! Jai Mahakal !"
 
 if __name__ == "__main__":
     app.run()
