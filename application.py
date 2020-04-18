d7e2618fd70a7e4db449369653cf379c42c1083b
manohar singh rajawat <manoharsingh1920@gmail.com>
2020-04-19 01:15:02 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    message = "! Jai Mahakal !"
+    return render_template('index.html',message=message)
 
 if __name__ == "__main__":
     app.run()
