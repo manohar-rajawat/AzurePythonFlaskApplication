c765543451e65cfa4dda179d46e5304b0a5c8192
manohar singh <manoharsingh1920@gmail.com>
2020-04-24 00:23:13 +0530

    application.py

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template,redirect,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+ return "Jai Mahakal"
+
+if __name__ == "__main__":
+ app.run(host="0.0.0.0")
